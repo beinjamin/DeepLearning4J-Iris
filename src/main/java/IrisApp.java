5749bc2dbd4a4643f7149e10a367ac02d55e26e0
beinjamin <jauresbeinjamin@gmail.com>
2022-04-27 20:48:28 +0100

    Mise en place de la vitesse d'apprentissage

--- src/main/java/IrisApp.java
+++ src/main/java/IrisApp.java
@@ -1,11 +1,15 @@
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.nd4j.linalg.learning.config.Adam;
 
 public class IrisApp {
 	
 	public static void main(String[] args) {
-	
+	double learninRate=0.001;
 	MultiLayerConfiguration configuration=new NeuralNetConfiguration.Builder()
+	.updater(new Adam(learninRate))
+			
+			
 	.list()
 	.build();
 	}
