4c74c67073d17527ecf9478a289c0ee1beb8e34d
beinjamin <jauresbeinjamin@gmail.com>
2022-04-27 20:54:16 +0100

    Finalisation avec le premier layer

--- src/main/java/IrisApp.java
+++ src/main/java/IrisApp.java
@@ -1,16 +1,26 @@
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.learning.config.Adam;
 
 public class IrisApp {
 	
 	public static void main(String[] args) {
 	double learninRate=0.001;
+	int numInputs=4;
+	int numHidden=10;
 	MultiLayerConfiguration configuration=new NeuralNetConfiguration.Builder()
 	.updater(new Adam(learninRate))
 			
 			
 	.list()
+	 .layer(0 , new DenseLayer.Builder()
+			 .nIn(numInputs)
+			 .nOut(numHidden)
+			 .activation(Activation.SIGMOID)
+			 .build()
+			 )
 	.build();
 	}
 }
