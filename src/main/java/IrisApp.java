666c2466b806066c5d831888a094c2c5c28ce23d
beinjamin <jauresbeinjamin@gmail.com>
2022-04-27 20:46:34 +0100

    Configuration du model

--- src/main/java/IrisApp.java
+++ src/main/java/IrisApp.java
@@ -1,4 +1,12 @@
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 
 public class IrisApp {
-
+	
+	public static void main(String[] args) {
+	
+	MultiLayerConfiguration configuration=new NeuralNetConfiguration.Builder()
+	.list()
+	.build();
+	}
 }
